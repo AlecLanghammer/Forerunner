783042be2e1f682047f7fd6ec86523cebf210fab
Alec Langhammer <alanghammer@fusionalliance.com>
2019-10-15 14:20:18 -0400

    config change

---
+++ Forerunner/Forerunner/TestUtils/BaseTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forerunner.TestUtils
+{
+    public abstract class BaseTest
+    {
+        public void SetupTest(string browser)
+        {
+
+        }
+
+        public void EndTest()
+        {
+
+        }
+    }
+}
